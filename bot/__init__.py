b670cc84f640d4e5d799085631c3fcae5fd7f762
Rajesh Kannan M <rajeshkannan.offl@gmail.com>
2023-01-12 23:45:15 +0530

    Emoji UI Added Added Emoji UI. Users can use Emoji UI by set it as True in config file. Added init file to understand process whether it is started or not. Connect with Dev https://rajeshkannanoffl.t.me

---
+++ bot/__init__.py
@@ -0,0 +1,1 @@
+print("Bot Started!!!.....")
--- bot/__main__.py
+++ bot/__main__.py
@@ -1,70 +1,98 @@
 import os
+import psutil
 import telebot
+import subprocess
 from dotenv import load_dotenv
 
-# create config.env in root-folder (and add API_KEY=<token>)
+# data to be entered in config.env loacted at root-folder
 # add bot-token in API_KEY
-load_dotenv('config.env', override=True)            
+# add True for emoji ui deployment
+load_dotenv('config.env', override=True)
 API_KEY = os.getenv("API_KEY")
-
+EMOJI_UI = os.getenv("EMOJI_UI")
 
 # connecting to the telegram bot
 bot = telebot.TeleBot(API_KEY)
 
-# start and help command
-@bot.message_handler(commands=['start','help'])
+# start command (/start)
+@bot.message_handler(commands=['start'])
+def start(message):
+    if EMOJI_UI=="True":
+        ui_msg = '''
+Hello Mate, ✌️
+→ Welcome to Server Monitoring Bot. 🤖
+→ With me you can get to know the status of your Server, VPS, VM or even your PC/Laptop.
+[👨‍💻 Connect with Dev @rajeshkannanoffl 🙋‍♂️]
+        '''
+        bot.send_message(message.chat.id, ui_msg)
+    else:
+        msg = '''
+Hello Mate,
+→ Welcome to Server Monitoring Bot.
+→ With me you can get to know the status of your Server, VPS, VM or even your PC/Laptop.
+[Connect with Dev @rajeshkannanoffl]
+        '''
+        bot.send_message(message.chat.id, msg)
+
+# help command (/help)
+@bot.message_handler(commands=['help'])
 def help(message):
     msg = '''
-Server Monitoring Bot
--------------------------------
-0. Check Server Status → /check
-1. Disk Usage → /disk
-2. CPU and RAM Usage → /sysinfo
-3. Uptime Server → /uptime
-4. Server Description → /server
-5. Help → /help
-With Regards,
-Rajesh Kannan M
-(@rajeshkannanoffl)
---------------------------------
+Hi, I'm Server Monitoring Bot.
+Here are some Commands you can send me,
+/status → Health of the Server.
+/disk → Disk Usage Details (in GB).
+/sysinfo → CPU (%) and RAM Usage (in GB).
+/uptime → Uptime of the Server.
+/serverinfo → Some Description of the Server.
+/help → I'm open to Help You.
     '''
     bot.send_message(message.chat.id, msg)
 
-# make command function to chat if command receive
-@bot.message_handler(commands=['check'])
+# up status (/status)
+@bot.message_handler(commands=['status'])
 def check(message):
-    bot.send_message(message.chat.id, "The server is alive")
+    if EMOJI_UI=="True":
+        ui_msg = '''
+Hey, My Health is Good 🟢.
+Stay Safe 😷, Stay Healthy ❤️.
+        '''
+        bot.send_message(message.chat.id, ui_msg)
+    else:
+        msg = '''
+Hey, My Health is Good.
+Stay Safe, Stay Healthy.
+        '''
+        bot.send_message(message.chat.id, msg)
 
-
-# get system info
-
-import psutil
-import subprocess
-
-# make command function to chat if command receive
-@bot.message_handler(commands=['check'])
-def check(message):
-    bot.send_message(message.chat.id, "The server is alive")
-
-# disk usage (/disk)
+# disk usage (/disk) in GB
 @bot.message_handler(commands=['disk'])
 def disk(message):
-    diskTotal = int(psutil.disk_usage('/').total/(1024*1024*1024))
+    diskTotal = int(psutil.disk_usage('/').total/(1024*1024*1024))      # kb*mb*gb
     diskUsed = int(psutil.disk_usage('/').used/(1024*1024*1024))
     diskAvail = int(psutil.disk_usage('/').free/(1024*1024*1024))
     diskPercent = psutil.disk_usage('/').percent
 
-    msg = '''
-Disk Info
----------
-Total = {} GB
-Used = {} GB
-Avail = {} GB
-Usage = {} %\n'''.format(diskTotal,diskUsed,diskAvail,diskPercent)
-    bot.send_message(message.chat.id,msg)
+    if EMOJI_UI=="True":
+        ui_msg = '''
+╭─《 💾 DISK INFO 💾 》
+├ 💽 Total = {} GB
+├ 🗒️ Used = {} GB
+├ 🆓 Available = {} GB
+╰ 🧻 Usage = {} %
+        '''.format(diskTotal,diskUsed,diskAvail,diskPercent)
+        bot.send_message(message.chat.id,ui_msg)
+    else:
+        msg = '''
+╭─《 DISK INFO 》
+├ Total = {} GB
+├ Used = {} GB
+├ Available = {} GB
+╰ Usage = {} %
+        '''.format(diskTotal,diskUsed,diskAvail,diskPercent)
+        bot.send_message(message.chat.id,msg)
 
-
-# cpu & ram (/sysinfo)
+# cpu & ram (/sysinfo) (cpu in percent & ram in GB)
 @bot.message_handler(commands=['sysinfo'])
 def sysinfo(message):
     cpuUsage = psutil.cpu_percent(interval=1)
@@ -72,39 +100,73 @@ def sysinfo(message):
     ramUsage = int(psutil.virtual_memory().used/(1024*1024*1024)) #GB
     ramFree = int(psutil.virtual_memory().free/(1024*1024*1024)) #GB
     ramUsagePercent = psutil.virtual_memory().percent
-    msg = '''
-CPU & RAM Info
----------
-CPU Usage = {} %
-RAM
-Total = {} GB
-Usage = {} GB
-Free  = {} GB
-Used = {} %\n'''.format(cpuUsage,ramTotal,ramUsage,ramFree,ramUsagePercent)
-    bot.send_message(message.chat.id,msg)
+
+    if EMOJI_UI=="True":
+        ui_msg = '''
+╭─《 🛠️ CPU INFO 🛠️ 》
+├ 🖥️ CPU Usage = {} %  
+│
+├《 💠 RAM INFO 💠 》
+├ 💿 Total = {} GB     
+├ 🗒️ Used = {} GB      
+├ 🆓 Available = {} GB 
+╰ 🧻 Usage = {} %
+        '''.format(cpuUsage,ramTotal,ramUsage,ramFree,ramUsagePercent)
+        bot.send_message(message.chat.id,ui_msg)
+    else:
+        msg = '''
+╭─《 CPU INFO 》
+├ CPU Usage = {} %  
+│
+├《 RAM INFO 》
+├ Total = {} GB     
+├ Used = {} GB      
+├ Available = {} GB 
+╰ Usage = {} %
+        '''.format(cpuUsage,ramTotal,ramUsage,ramFree,ramUsagePercent)
+        bot.send_message(message.chat.id,msg)
+    
 
 # uptime (/uptime)
 @bot.message_handler(commands=['uptime'])
 def uptime(message):
     upTime = subprocess.check_output(['uptime','-p']).decode('UTF-8')
-    msg = upTime
-    bot.send_message(message.chat.id,msg)
+    if EMOJI_UI=="True":
+        ui_msg = '''
+⌛ {}
+        '''.format(upTime)
+        bot.send_message(message.chat.id,ui_msg)
+    else:
+        msg = '''
+{}
+        '''.format(upTime)
+        bot.send_message(message.chat.id,msg)
 
 
-# server desc (/server)
-@bot.message_handler(commands=['server'])
+# server desc (/serverinfo)
+@bot.message_handler(commands=['serverinfo'])
 def server(message):
     uname = subprocess.check_output(['uname','-rsoi']).decode('UTF-8')
     host = subprocess.check_output(['hostname']).decode('UTF-8')
     ipAddr = subprocess.check_output(['hostname','-I']).decode('UTF-8')
-    msg ='''
-Server Desc
----------
-OS = {}
-Hostname = {}
-IP Addr = {}'''.format(uname,host,ipAddr)
-    bot.send_message(message.chat.id,msg)
 
+    if EMOJI_UI=="True":
+        ui_msg = '''
+《 ⚙️ SERVER INFO ⚙️ 》
+
+☢️ OS = {} 
+🆔 Hostname = {}
+🔑 IP Address = {}
+        '''.format(uname,host,ipAddr)
+        bot.send_message(message.chat.id,ui_msg)
+    else:
+        msg = '''
+《 SERVER INFO 》
+OS = {} 
+Hostname = {}
+IP Address = {}
+        '''.format(uname,host,ipAddr)
+        bot.send_message(message.chat.id,msg)
 
 # listen to telegram commands
 bot.polling()
